7c0eb8974bb6429884ebf9d005354aa0d936cb0d
alessiovignoli <45239231+alessiovignoli@users.noreply.github.com>
2021-07-15 12:32:57 +0200

    Add files via upload

--- scripts_python/plp_average_computer.py
+++ scripts_python/plp_average_computer.py
@@ -33,13 +33,24 @@ def plp_averager(single_plp_file, field_keyword, range_of_aa=False):
 
 
 
-def multi_plp_averager(in_pred_txt, multi_plp_file, field_keyword, number_max_iter=False):
+def multi_plp_averager(in_pred_txt, multi_plp_file, field_keyword, number_max_iter=False, secondary_field_keywords=False):
     #print(in_pred_txt, multi_plp_file, field_keyword, number_max_iter)
     check_keyword = {"i":2, "o":3, "-":4, "n":4, "c":5, "s":6, "l":7}
     if field_keyword not in check_keyword:
         print('please give a keyword for selecting the field \nit can be passed from command line --KEYWORD in nextflow or with third argument in python', file=sys.stderr)
         print("the allowed keywords are: [c, i, o, -, n, s, l] \n c = signal peptide, i = inside membrane(cytoplasm), o = outside membrane, - = helix (in phobius originalmodel) \n (only in phobius-M7or later) => -n- = normal-helix  -s- = special-helix and -l- = loop-inramembrane", file=sys.stderr)
         raise SystemExit
+    print(field_keyword, end='')
+    look_other_columns = False
+    if secondary_field_keywords != False:
+        for sec_keyword in secondary_field_keywords:
+            if sec_keyword not in check_keyword:
+                print('please give a set of keywords separated by a comma for selecting the others field to look for \nit can be passed from command line --SEC_KEYWORD in nextflow or with fifth argument in python', file=sys.stderr)
+                print("the allowed keywords are: [c, i, o, -, n, s, l] \n c = signal peptide, i = inside membrane(cytoplasm), o = outside membrane, - = helix (in phobius originalmodel) \n (only in phobius-M7or later) => -n- = normal-helix  -s- = special-helix and -l- = loop-inramembrane", file=sys.stderr)
+                raise SystemExit
+        look_other_columns = True
+        print('', secondary_field_keywords, 'protein_id start end max_vaues')
+    #print(look_other_columns, secondary_field_keywords)
     with open(in_pred_txt, 'r') as intxt, open(multi_plp_file, 'r') as inplp:
         first_iter_counter = True
         next_header = ''
@@ -63,39 +74,71 @@ def multi_plp_averager(in_pred_txt, multi_plp_file, field_keyword, number_max_it
                     columns = inplp.readline()
                 #print('header:',header, end='')
                 #print('columns :',columns, end='')
-                summatory = 0.0
-                #summatory1 = 0.0        # comment it
-                max_plp = 0.0
+                summatory = [[0.0]]
+                max_plp = [[0.0]]
+                if look_other_columns:
+                    for elem in secondary_field_keywords:
+                        summatory.append([0.0])
+                        max_plp.append([0.0])
+                aa_num = 0
                 if seq_id in header:
                     for plpline in inplp:
                         #print(plpline, end='')
                         if '#' in plpline:
                             next_header = plpline
                             #print('next_header:',next_header, end='')
-                            break
+                            if list_boundaries == []:
+                                break
+                            else:
+                                #print(summatory, max_plp)
+                                tmp1 = []
+                                for n in range (0,len(summatory)):
+                                    average_plp = summatory[n][0] / (aa_num - left_extr + 1)
+                                    print(average_plp, '',  end='')
+                                    summatory[n][0] = 0.0
+                                    tmp1.append(max_plp[n][0])
+                                    max_plp[n][0] = 0.0
+                                print(seq_id, list_boundaries[-1], tmp1)
+                                list_boundaries.pop()
+                                iter_num += 1
+                                break
                         else:
                             aa_num = int(plpline.split()[0])
                             #print(aa_num, list_boundaries)
                             if list_boundaries == []:
                                 continue
                             else:
-                                if aa_num >= float(list_boundaries[-1][0]) and aa_num <= float(list_boundaries[-1][1]):
+                                left_extr = int(list_boundaries[-1][0])
+                                right_extr = None
+                                if list_boundaries[-1][1] == 'inf':
+                                    right_extr = 9999999999
+                                else:
+                                    right_extr = int(list_boundaries[-1][1])
+                                if aa_num >= left_extr and aa_num <= right_extr:
                                     #print(plpline, end='')
-                                    requested_field = float((plpline.split())[check_keyword[field_keyword]]) #+ float((plpline.split())[4])
-                                    #requested_field1 = float((plpline.split())[4])   # comment it
+                                    requested_field = float((plpline.split())[check_keyword[field_keyword]])
                                     #print(aa_num, requested_field)
-                                    summatory += requested_field
-                                    #summatory1 += requested_field1     # comment it
+                                    summatory[0][0] += requested_field
                                     #print(summatory)
-                                    max_plp = max(max_plp, requested_field)
-                                    if aa_num == list_boundaries[-1][1]:
-                                        average_plp = summatory / (list_boundaries[-1][1] - list_boundaries[-1][0] + 1)
-                                        #average_plp1 = summatory1 / (list_boundaries[-1][1] - list_boundaries[-1][0] + 1)   # comment it
-                                        print(average_plp, seq_id, list_boundaries[-1], max_plp)       # uncomment it
-                                        #print(average_plp, average_plp1, seq_id, list_boundaries[-1], max_plp)        # comment it
+                                    max_plp[0][0] = max(max_plp[0][0], requested_field)
+                                    if look_other_columns:
+                                        i = 1
+                                        for sec_key in secondary_field_keywords:
+                                            tmp = float((plpline.split())[check_keyword[sec_key]])
+                                            summatory[i][0] += tmp
+                                            max_plp[i][0] = max(max_plp[i][0], tmp)
+                                            i += 1
+                                    if aa_num == right_extr:
+                                        #print(summatory, max_plp)
+                                        tmp1 = []
+                                        for n in range (0,len(summatory)):
+                                            average_plp = summatory[n][0] / (right_extr - left_extr + 1)
+                                            print(average_plp, '',  end='')
+                                            summatory[n][0] = 0.0
+                                            tmp1.append(max_plp[n][0])
+                                            max_plp[n][0] = 0.0
+                                        print(seq_id, list_boundaries[-1], tmp1)
                                         list_boundaries.pop()
-                                        summatory = 0.0
-                                        #summatory1 = 0.0
                                         iter_num += 1
                         if number_max_iter != False and iter_num == number_max_iter:
                             break
@@ -131,14 +174,17 @@ if __name__ == "__main__":
                 field_keyword = sys.argv[3]
                 phobius_short_stdout_txt_file = sys.argv[4]
                 num_max_iterations = int(sys.argv[5])
+                secondary_field_keywords = sys.argv[6].split(',')
             except:
-                if phobius_short_stdout_txt_file != None:
+                if num_max_iterations != None:
+                    multi_plp_averager(phobius_short_stdout_txt_file, plp_file, field_keyword, num_max_iterations)
+                elif phobius_short_stdout_txt_file != None:
                     multi_plp_averager(phobius_short_stdout_txt_file, plp_file, field_keyword)
                 else:
-                    print('Program usage: text.py < a mandatory string argument that can have as arguments either "single" or "multi" , this flag changes the behaviour of the script from using  the function plp_averager with single or function multi_plp_averager with multii, in this case is multi> < the plp file to be used> <the field keyword that has to be used to retrieve the column of the plp of the phobius prediction for such label> < an optional field for the maxumum number of iterations, iterations are intended as number of ranges found by the phobius_short_pred_field_selecter funtion, so if ranges for tm helices are selected an iteration is a single tm helix not a protein>', file=sys.stderr)
+                    print('Program usage: text.py < a mandatory string argument that can have as arguments either "single" or "multi" , this flag changes the behaviour of the script from using  the function plp_averager with single or function multi_plp_averager with multii, in this case is multi> < the plp file to be used> <the field keyword that has to be used to retrieve the column of the plp of the phobius prediction for such label> < an optional field for the maxumum number of iterations, iterations are intended as number of ranges found by the phobius_short_pred_field_selecter funtion, so if ranges for tm helices are selected an iteration is a single tm helix not a protein> < one or more field keywords separated by a comma, this option is used when for example the selected main field keyword is s (special helix) but we want to compute also the average for the normal helix colum and loop for that predicted special helix segment, so in this case it has to specified as n,l  if we were interested only in n it would be n >', file=sys.stderr)
                     raise SystemExit
             else:
-                multi_plp_averager(phobius_short_stdout_txt_file, plp_file, field_keyword, num_max_iterations)
+                multi_plp_averager(phobius_short_stdout_txt_file, plp_file, field_keyword, num_max_iterations, secondary_field_keywords)
         else:
             print('Program usage: text.py < a mandatory string argument that can have as arguments either "single" or "multi" , this flag changes the behaviour of the script from using  the function plp_averager with single or function multi_plp_averager with multi> The argument passed is not allowed, it has been passed: ', switch, file=sys.stderr)
             raise SystemExit
