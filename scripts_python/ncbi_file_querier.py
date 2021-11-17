04edac1f8f696fa99c51d09c880d49819648fc1f
alessiovignoli <45239231+alessiovignoli@users.noreply.github.com>
2021-11-17 17:16:04 +0100

    Add files via upload

--- scripts_python/ncbi_file_querier.py
+++ scripts_python/ncbi_file_querier.py
@@ -2,56 +2,73 @@
 
 import sys
 
-def fasta_header_tax_id_ncbi_query (in_fasta, in_ncbi, not_found_verbose=False):
-    print(in_fasta, in_ncbi, not_found_verbose)
+def ncbi_categories_db_formatter (in_ncbi):
+    list_ncbi_db = []
+    with open(in_ncbi, 'r') as in_db:
+        for db_line in in_db:
+            kingdom_key = db_line.split()[0]
+            species_id_string = db_line.rstrip().split()[2] #+ ' '
+            check = False
+            number_sublist = 0
+            for num_sublist in range(0, len(list_ncbi_db)):
+                #print("sublist :", list_ncbi_db[num_sublist], "  index", num_sublist)
+                if kingdom_key in list_ncbi_db[num_sublist]:
+                    check = True
+                    number_sublist = num_sublist
+            if check:
+                list_ncbi_db[number_sublist][1].append(species_id_string)
+            else:
+                list_ncbi_db.append([kingdom_key, [species_id_string]])
+        #print(list_ncbi_db[0][0], "\t", list_ncbi_db[1][0], "\t", list_ncbi_db[2][0], "\t", list_ncbi_db[3][0], "\t", list_ncbi_db[4][0], "\t", list_ncbi_db[5][0])
+        #print(len(list_ncbi_db[0][1]), len(list_ncbi_db[1][1]), len(list_ncbi_db[2][1]), len(list_ncbi_db[3][1]), len(list_ncbi_db[4][1]), len(list_ncbi_db[5][1]))
+    return list_ncbi_db
 
-"""    count_dict = {}
-    with open(in_ids, 'r') as inheader, open(in_fasta, 'r') as infasta:
-        tot_count= 0
-        species_id = 0
-        tmp = None
-        for line in inheader:
-            seq_id = line.split()[0]
-            for line2 in infasta:
-                tmp = seq_id
-                if line2[0] == '>' and seq_id in line2:
-                    species_id = None
-                    if 'TaxID=' in line2:
-                        species_id = line2.split('TaxID=')[1].split(' ')[0]
-                    elif 'Taxid=' in line2:
-                        species_id = line2.split('Taxid=')[1].split(' ')[0]
-                    elif 'taxid=' in line2:
-                        species_id = line2.split('taxid=')[1].split(' ')[0]
-                    elif 'taxID=' in line2:
-                        species_id = line2.split('taxID=')[1].split(' ')[0]
-                    elif 'TaxID =' in line2:
-                        species_id = line2.split('TaxID =')[1].split(' ')[0]
-                    elif 'OX=' in line2:
-                        species_id = line2.split('OX=')[1].split(' ')[0]
-                    elif 'ox=' in line2:    
-                        species_id = line2.split('ox=')[1].split(' ')[0]
-                    elif 'OX =' in line2:
-                        species_id = line2.split('OX =')[1].split(' ')[0]
-                    else:
-                        print('there is no tax id field identifier tried in this script in the header, please check if there is TaxID= or OX= in the headers, this is the problematic line', line2,  file=sys.stderr)
-                        raise SystemExit
-                    print(seq_id, species_id)
-                    break
-        if species_id == 0:
-            print('this is id has not been found in the fasta file :', tmp, file=sys.stderr)
-"""
+
+def fasta_ncbi_querier (in_fasta, list_ncbi, not_found_verbose=False)
+    list_of_accepted_taxid_keywords = ['TaxID=', 'Taxid=', 'taxid=', 'taxID=', 'TaxID =', 'OX=', 'ox=', 'OX =']
+    with open(in_fasta, 'r') as fasta:
+        for fasta_line in fasta:
+            if fasta_line[0]=='>':
+                presence_taxid_keyword = False
+                print(fasta_line, end='')
+                for taxid_keyword in list_of_accepted_taxid_keywords:
+                    if taxid_keyword in fasta_line:
+                        #print("taxid keyword:", taxid_keyword)
+                        tmp = fasta_line.split(taxid_keyword)[1].lstrip()
+                        species_id = ''
+                        presence_taxid_keyword = True
+                        for digit in tmp:
+                            if digit.isnumeric():
+                                species_id += digit
+                            else:
+                                break
+                        print("species id :", species_id)
+                        for n_sublist in range(0, len(list_ncbi)):
+                            if species_id in list_ncbi[n_sublist][1]:
+                                print(species_id, list_ncbi_db[n_sublist][0])
+                        break
+                #print(presence_taxid_keyword)
+                if presence_taxid_keyword==False:
+                    print("the taxid keyworfd used in this line is not present in the possible ones that the script looks for, if you want add it at line 112 in the list called <list_of_accepted_taxid_keywords>\n", "the line inquired is:\n", fasta_line, "\nthe possible taxids searched by this file are:\n", list_of_accepted_taxid_keywords, file=sys.stderr)
+                    raise SystemExit
+            else:
+                continue
+
 
 if __name__ == "__main__":
     in_database  = None
     try:
         infasta = sys.argv[1]
-	    in_database = sys.argv[2]
-	    silencer = sys.argv[3]
+        in_database = sys.argv[2]
+        silencer = sys.argv[3]
     except:
         if in_database != None:
-            fasta_header_tax_id_ncbi_query(infasta, in_database)
+            formatted_categories = ncbi_categories_db_formatter(infasta, in_database)
+            fasta_ncbi_querier(infasta, formatted_categories)
         else:
             print('Program usage: text.py <a fasta file that have in the header (the line called header has to start with >) and again in the header the tax id associated to such id/sequence, the format is actually the one of Uniprot with OX=taxid_num or the Uniref format with TaxID=tax_num and variation of them lice all lower case ecc. to see them take a look at the actual code if sections.> < a ncbi downloaded file with three columd tab separsted, with first column is the kingdom one letter identifier, the second is astrand taxid, and the third the species id (both fields are checked for a match) >\n  WARNING \n the script spits out the ids that are not fond, with replicas, if it does not find 849 every time it encounters it along the inputfastalike it will print it, if you want to avoid this behaviour just add a third field with false', file=sys.stderr)
             raise SystemExit
     else:
-        fasta_header_tax_id_ncbi_query(infasta, in_database, silencer)
+        formatted_categories = ncbi_categories_db_formatter(infasta, in_database)
+        fasta_ncbi_querier(infasta, formatted_categories, silencer)
+
