62b5b94df87eab99bc8d8f30b4b964c04e5ab124
alessiovignoli <45239231+alessiovignoli@users.noreply.github.com>
2022-01-19 11:58:57 +0100

    Add files via upload

--- scripts_python/adjacent_annotation_finder.py
+++ scripts_python/adjacent_annotation_finder.py
@@ -44,10 +44,23 @@ def  adjacent_annot_finder(in_domain_info, in_shortpred, input_field, prefix):
                     for annotation in domain_line.split('[')[1:]:
                         for i, left_extr in enumerate(annotation.split(';')[2::2]):
                             #print('i:', i, ' left extremity:', left_extr)           ## i has to be multiplied by two to represent the index
-                            right_extr = int((annotation.split(';')[2:][(i*2+1)]).split(']')[0])  ## it can end with the closed square brachet
+                            right_extr = None
+                            try:
+                                right_extr = int((annotation.split(';')[2:][(i*2+1)]).split(']')[0]) # it can end with the closed square brachet 
+                                expedient = int(left_extr)		# it is rare but a comma might be inside the annotation name in this case
+                                                                        # the script would crash and the best thing is go with the following code that's skips the annotation
+                            except Exception:
+                                #print(domain_line)			# this whole except section id made to catch the error given by a
+                                #print('annotation:', annotation)	# open square brachet inside the annotation name
+                                #print('boundary', boundary)		# solution is not to compute anything in that scenario 
+                                #print('i:', i, ' left extremity:', left_extr)
+                                #raise SystemExit
+                                break
+                            else:
+                                pass
                             if int(left_extr) <= left_margin and right_extr >= right_margin:
                                 #print('0: ', left_extr, right_extr,  annotation.split(';')[1], left_margin, right_margin)
-                                center_adj.write((left_extr + ' ' + str(right_extr) + ' ' + str(left_margin) + ' ' + str(right_margin) + ' ' + annotation.split(';')[1] + '\n' ))
+                                center_adj.write((left_extr + ' ' + str(right_extr) + ' ' + str(left_margin) + ' ' + str(right_margin) + ' [' + annotation.split(';')[1] + '\n' ))
                             elif right_extr < (right_margin - range_uncertainty_value) and right_extr >= tmp_left_adjacent:
                                 tmp_left_adjacent = right_extr
                                 buffer1 = [left_extr, annotation.split(';')[1]]
@@ -58,9 +71,9 @@ def  adjacent_annot_finder(in_domain_info, in_shortpred, input_field, prefix):
                     #print('+1: ', tmp_right_adjacent, left_margin, right_margin)
                     #print(buffer1, buffer2)
                     if buffer1[1] is not None:
-                        left_adj.write((buffer1[0] + ' ' + str(tmp_left_adjacent) + ' ' + str(left_margin) + ' ' + str(right_margin) +  ' ' + buffer1[1] + '\n' ))
+                        left_adj.write((buffer1[0] + ' ' + str(tmp_left_adjacent) + ' ' + str(left_margin) + ' ' + str(right_margin) +  ' [' + buffer1[1] + '\n' ))
                     if buffer2[1] is not None:
-                        right_adj.write((str(tmp_right_adjacent) + ' ' + buffer2[0]  + ' ' + str(left_margin) + ' ' + str(right_margin) + ' ' + buffer2[1] + '\n' ))
+                        right_adj.write((str(tmp_right_adjacent) + ' ' + buffer2[0]  + ' ' + str(left_margin) + ' ' + str(right_margin) + ' [' + buffer2[1] + '\n' ))
                 else:
                     #print('shortpred id:', boundary[2], '  domain_info id:', domain_line.split()[0])
                     continue
@@ -74,7 +87,7 @@ if __name__ == "__main__":
         input_field = sys.argv[3]
         prefix_output_name = sys.argv[4]
     except Exception:
-        print('Program usage: text.py <a domain_info file that has on each line a protein annotation in a special format,\nlike:\nUPI00068AA065 [IPR036514;sgnh hydrolase superfamily;471;634] [IPR002656;acyltransferase 3 domain;5;321] [IPR043968;sgnh domain;400;626]\nwhere the first field is the protein id and then there are as many lists as there have been found annotations for this entry in uniprot, there is the keyword associted with each feature a word descriptive feature and the extremities of the feature, there can be more of them\nto generate this file the script   uniprot_rest_query.py     or the pipeline     kingdom_freq_computer.nf      look into those for more details> <the second file is the short output of phobius, where on each line is present the prediction and position of the latter on the sequence, with this info the neighbouring annotations are found,\n####  WARNING  #####\n the ids are suppurted only in uniref and uniprot format, in the sense that they are split on _ > <a mandatory field letter for knowing which feature predicted to look at in short pred if you do not know what to put take alook at      phobius_short_prediction_field_retriever.py    eror messages> < a prefix for the three output files that are going to be written each one representing a type of adjacent annotation, mainly closest to left, incorporating, closest to the right they can not be present in a pgiven pro>', file=sys.stderr)
+        print('Program usage: text.py <a domain_info file that has on each line a protein annotation in a special format,\nlike:\nUPI00068AA065 [IPR036514;sgnh hydrolase superfamily;471;634] [IPR002656;acyltransferase 3 domain;5;321] [IPR043968;sgnh domain;400;626]\nwhere the first field is the protein id and then there are as many lists as there have been found annotations for this entry in uniprot, there is the keyword associted with each feature a word descriptive feature and the extremities of the feature, there can be more of them\nto generate this file the script   uniprot_rest_query.py    look into that for more details> <the second file is the short output of phobius, where on each line is present the prediction and position of the latter on the sequence, with this info the neighbouring annotations are found,\n####  WARNING  #####\n the ids are suppurted only in uniref and uniprot format, in the sense that they are split on _ > <a mandatory field letter for knowing which feature predicted to look at in short pred if you do not know what to put take alook at      phobius_short_prediction_field_retriever.py    eror messages> < a prefix for the three output files that are going to be written each one representing a type of adjacent annotation, mainly closest to left, incorporating, closest to the right they can not be present in a pgiven pro>', file=sys.stderr)
         raise SystemExit
     else:
         adjacent_annot_finder(input_domain_info_file, input_phobius_shortpred, input_field, prefix_output_name)
